ee64ece8d64d3e7ff84a0aa6ac4e9720acdadd86
Mittali5 <146563140+Mittali5@users.noreply.github.com>
2023-10-09 20:35:27 +0530

    Create script.js
    add js

---
+++ script.js
@@ -0,0 +1,13 @@
+let screenValue = document.getElementById("calc")
+
+function clearAll(){
+    screenValue.value=" "
+}
+
+function display(value){
+    screenValue.value += value
+}
+
+function calculate(){
+    screenValue.value =eval(screenValue.value)
+}
